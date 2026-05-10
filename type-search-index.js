9cc994f0668ff5a17c112280bac97429f2ae01e2
brbriandrm <om31.artem.lytvynenko@gmail.com>
2026-05-10 10:22:06 +0000

    Deploying to gh-pages from @ brbriandrm/Lab6@b8f6945b6ddc2be5edb7ad8101118b75b24c5503 🚀

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Tariff"},{"p":"<Unnamed>","l":"TariffSingleLinkedList"}];updateSearchResults();

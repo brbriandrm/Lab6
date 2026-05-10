b8f6945b6ddc2be5edb7ad8101118b75b24c5503
Artem Lytvynenko <om31.artem.lytvynenko@gmail.com>
2026-05-10 13:21:07 +0300

    feat: add the rest of the list interface methods
    create Main class

--- src/main/java/TariffSingleLinkedList.java
+++ src/main/java/TariffSingleLinkedList.java
@@ -1,5 +1,7 @@
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class TariffSingleLinkedList implements List<Tariff> {
 
@@ -53,17 +55,75 @@ public class TariffSingleLinkedList implements List<Tariff> {
     }
 
     @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<Tariff> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] ts) {
+        return null;
+    }
+
+    @Override
     public boolean add(Tariff t){
         Node newNode = new Node(t);
         if(head == null) {
             head = newNode;
+            size++;
+            return true;
         }
         Node current = head;
         while(current.getNext() != null) {
             current = current.getNext();
         }
         current.next = newNode;
+        size++;
         return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Tariff> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int i, Collection<? extends Tariff> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> collection) {
+        return false;
+    }
+
+    public void clear() {
+        head = null;
+        size = 0;
     }
 
     @Override
@@ -76,6 +136,16 @@ public class TariffSingleLinkedList implements List<Tariff> {
             current = current.getNext();
         }
         return current.data;
+    }
+
+    @Override
+    public Tariff set(int i, Tariff tariff) {
+        return null;
+    }
+
+    @Override
+    public void add(int i, Tariff tariff) {
+
     }
 
     @Override
@@ -100,6 +170,31 @@ public class TariffSingleLinkedList implements List<Tariff> {
         size -= 1;
         return current.data;
 
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<Tariff> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<Tariff> listIterator(int i) {
+        return null;
+    }
+
+    @Override
+    public List<Tariff> subList(int i, int i1) {
+        return List.of();
     }
 
 

e83d3e0e890220ac7f1773ba0053cd3b8c18a215
Artem Lytvynenko <om31.artem.lytvynenko@gmail.com>
2026-05-10 15:14:13 +0300

    Add javadocs website

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"LinkedList"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Tariff"},{"p":"<Unnamed>","l":"TariffSingleLinkedList"},{"p":"<Unnamed>","l":"TariffSingleLinkedListTest"}];updateSearchResults();
